353319c36c1a4a8fffd5359e23a9addd824f5761
selimreza <mikhailsm174@gmail.com>
2024-11-05 20:36:23 -0800

    new project upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const formCntrl = document.querySelector('#form-cntrl');
+
+
+
+
+function validateEmail(email){
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+
+  formCntrl.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+  })
